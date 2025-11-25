df9ce07442d18ce09b3f32fbd44f8d7ead6a288c
Aman Shaw <amanshaw4511@protonmail.com>
2025-11-25 23:27:20 +0530

    update hash

--- Formula/sctl.rb
+++ Formula/sctl.rb
@@ -1,8 +1,8 @@
 class Sctl < Formula
   desc "systemctl command made easy with fzf"
   homepage "https://github.com/amanshaw4511/sctl"
-  url "https://github.com/amanshaw4511/sctl"
-  sha256 ""
+  url "https://github.com/amanshaw4511/sctl/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "6f18272f7f6a551dc3743dd573f51fca5d0c30eefffcd75c0fe451dd803e7e2d"
   licence "GPL-3"
 
   depends_on "fzf"
